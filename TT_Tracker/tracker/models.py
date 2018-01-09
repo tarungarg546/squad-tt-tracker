d19a4a9066cbfd2d3074784a5190873da1c740d9
Kishan <arun@squadrun.co>
2018-01-09 19:26:16 +0530

    Changes to kat_#154124858 2

--- TT_Tracker/tracker/models.py
+++ TT_Tracker/tracker/models.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import F
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.utils.functional import cached_property
@@ -11,7 +10,7 @@ from django.contrib.auth.models import User
 
 
 class Team(models.Model):
-    team_name = models.CharField(max_length=30, unique=True, help_text='If Singles, enter same name as user 1 to avoid confusion')
+    team_name = models.CharField(max_length=30, unique=True, help_text='Enter as Text. Maximum Length:30')
     user_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='firstplayers')
     user_2 = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='secondplayers', help_text='If Singles, leave this empty')
 
@@ -20,29 +19,30 @@ class Team(models.Model):
 
 
 class Match(models.Model):
-    singles_value = 0
-    doubles_value = 1
+    SINGLES_VALUE = 0
+    DOUBLES_VALUE = 1
     CATEGORIES = (
-        (singles_value, 'Singles'),
-        (doubles_value, 'Doubles'),
+        (SINGLES_VALUE, 'Singles'),
+        (DOUBLES_VALUE, 'Doubles'),
     )
 
-    match_name = models.CharField(max_length=62, blank=True, null=True)
     category = models.PositiveSmallIntegerField(choices=CATEGORIES)
-    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='firstteams', help_text='Choose team 1 of your category')
-    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='secondteams', help_text='Choose team 2 of your category')
+    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='firstteams', help_text='object in Teams DB')
+    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='secondteams', help_text='object in Teams DB')
     winner = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='winners', blank=True, null=True)
     started_at = models.DateTimeField(default=timezone.now)
     ended_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.match_name
+        return self.id
 
     def save(self, *args, **kwargs):
-        self.match_name = self.team_1.team_name + 'VS' + self.team_2.team_name
         games_of_match = self.game_set.all()
         total_games = len(games_of_match)
-        team_1_wins = len(games_of_match.filter(team_1_score__gt=F('team_2_score')))
+        team_1_wins = 0
+        for game in games_of_match:
+            if game.team_1_score > game.team_2_score:
+                team_1_wins += 1
         if team_1_wins > total_games/2:
             self.winner = self.team_1
         else:
@@ -62,11 +62,9 @@ class Match(models.Model):
            self.team_1.user_2 == self.team_2.user_1 or self.team_1.user_2 == self.team_2.user_2)
            ):
             raise ValidationError(_('One of the users is on both teams!'))
-        # Find the winner if winner field is empty
-        # self.find_winner()
 
 
 class Game(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
-    team_1_score = models.IntegerField(default=0)
-    team_2_score = models.IntegerField(default=0)
+    team_1_score = models.IntegerField(help_text='Enter in Integer Format')
+    team_2_score = models.IntegerField(help_text='Enter in Integer Format')
