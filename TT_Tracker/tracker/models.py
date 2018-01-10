6c2e5c9aad76794ec272a1cfdae115615b870a40
Kishan <arun@squadrun.co>
2018-01-10 11:51:31 +0530

    First Commit to kat_#154124863_adminMods
    Changes to admin to view as well as create easily. Fixed problem with typeerror on returning match id

--- TT_Tracker/tracker/models.py
+++ TT_Tracker/tracker/models.py
@@ -34,7 +34,7 @@ class Match(models.Model):
     ended_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
     def save(self, *args, **kwargs):
         games_of_match = self.game_set.all()
