d815caeb05d27b78d51610fa6875bab3a8ea0fc3
Kishan <arun@squadrun.co>
2018-01-10 20:49:08 +0530

    Additions to users comparisons

--- TT_Tracker/tracker/views.py
+++ TT_Tracker/tracker/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Team, User
+from .models import Team, User, Match
 
 
 def index(request):
@@ -48,5 +48,43 @@ def compare_teams(request):
         }
         return render(request, 'tracker/compare_teams.html', context)
 
+
 def compare_users(request):
-    return render(request, 'tracker/compare_users.html')
+    if request.POST['user_1'] == request.POST['user_2']:
+        return redirect('tracker:index')
+    else:
+        post_user_1 = User.objects.get(id=request.POST['user_1'])
+        post_user_2 = User.objects.get(id=request.POST['user_2'])
+        all_matches = Match.objects.all()
+        user_1_wins = 0
+        user_2_wins = 0
+        matches_played = 0
+        for match in all_matches:
+            USER_1_IN_TEAM_1 = match.team_1.user_1 == post_user_1 or match.team_1.user_2 == post_user_1
+            USER_1_IN_TEAM_2 = match.team_2.user_1 == post_user_2 or match.team_2.user_2 == post_user_2
+            USER_2_IN_TEAM_1 = match.team_1.user_1 == post_user_2 or match.team_1.user_2 == post_user_2
+            USER_2_IN_TEAM_2 = match.team_2.user_1 == post_user_1 or match.team_2.user_2 == post_user_1
+
+            if (
+                    (USER_1_IN_TEAM_1 and USER_2_IN_TEAM_2) or (USER_1_IN_TEAM_2 and USER_2_IN_TEAM_1)):
+                matches_played += 1
+                if USER_1_IN_TEAM_1 and match.winner == match.team_1:
+                    user_1_wins += 1
+                else:
+                    user_2_wins += 1
+        if matches_played == 0:
+            user_1_win_ratio = 0
+            user_2_win_ratio = 0
+        else:
+            user_1_win_ratio = user_1_wins/matches_played
+            user_2_win_ratio = user_2_wins/matches_played
+        context = {
+            'user_1': post_user_1,
+            'user_2': post_user_2,
+            'matches_played': matches_played,
+            'user_1_wins': user_1_wins,
+            'user_2_wins': user_2_wins,
+            'user_1_win_ratio': user_1_win_ratio,
+            'user_2_win_ratio': user_2_win_ratio,
+        }
+        return render(request, 'tracker/compare_users.html', context)
